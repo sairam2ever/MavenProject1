7a4148d666c573d22100e2d04ffbf8e8258037f1
vatnaray <vatnaray@cisco.com>
2018-04-16 22:06:19 +0530

    fibonacci

--- Fibonacci.sh
+++ Fibonacci.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
- 
-a=1
- 
-b=1
- 
-echo $a
- 
-echo $b
- 
-for I in 1 2 3 4 5 6 7 8
- 
+#!/bin/bash#!/bin/bash
+# SCRIPT:  fibo_iterative.sh
+# USAGE:   fibo_iterative.sh [Number]
+# PURPOSE: Generate Fibonacci sequence.
+#                         \\\\ ////
+#                         \\ - - //
+#                            @ @
+#                    ---oOOo-( )-oOOo---
+#
+#####################################################################
+#                     Script Starts Here                            #
+#####################################################################
+
+if [ $# -eq 1 ]
+then
+    Num=$1
+else
+    echo -n "Enter a Number :"
+    read Num
+fi
+
+f1=0
+f2=1
+
+echo "The Fibonacci sequence for the number $Num is : "
+
+for (( i=0;i<=Num;i++ ))
 do
- 
-c=a
- 
-b=$a
- 
-b=$(($a+$c))
- 
-echo $b
- 
+     echo -n "$f1 "
+     fn=$((f1+f2))
+     f1=$f2
+     f2=$fn
 done
+
+echo
